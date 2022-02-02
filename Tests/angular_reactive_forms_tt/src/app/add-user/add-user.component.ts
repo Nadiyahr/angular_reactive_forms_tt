8282ed30f29ee799c6231b423138dd8e54b4881f
nadiaH <nadiahrb79@gmailcom>
2022-02-02 19:30:25 +0200

    formarray works

--- Tests/angular_reactive_forms_tt/src/app/add-user/add-user.component.ts
+++ Tests/angular_reactive_forms_tt/src/app/add-user/add-user.component.ts
@@ -28,28 +28,59 @@ export class AddUserComponent implements OnInit {
       lastName: new FormControl(this.user?.lastName),
       firstName: new FormControl(this.user?.firstName),
       middleName: new FormControl(this.user?.middleName),
-      carsControl: this.formBuilder.array(this.setCars())
+      carsControl: this.formBuilder.array([])
     })
 
-    console.log(this.setCars());
+    this.userCarsControl.setControl('carsControl', this.setExistingCars(this.cars ? this.cars : []))
+
+    // console.log(this.setCars());
     
 
-    this.userCarsControl.valueChanges.subscribe((value) => console.log(value));
+    // this.userCarsControl.valueChanges.subscribe((value) => console.log(value));
   }
+  
 
   get carsControl() {
     return this.userCarsControl.get('carsControl') as FormArray;
   }
 
-  setCars() {
-    if (this.cars) {
-      return this.cars.map(car => car);
-    } else {
-      return []
-    }
+  setExistingCars(cars: CarEntity[]): FormArray {
+    const formArray = new FormArray([]);
+    cars.forEach(car => {
+      formArray.push(
+        this.formBuilder.group({
+          plate: new FormControl(car.plate),
+          brand: new FormControl(car.brand),
+          model: new FormControl(car.model),
+          year: new FormControl(car.year),
+        })
+      )
+    })
+
+    return formArray;
+  }
+
+  // this.userCarsControl = this.formBuilder.group({
+  //   lastName: new FormControl(),
+  //   firstName: new FormControl(),
+  //   middleName: new FormControl(),
+  //   carsControl: this.formBuilder.array([
+  //     plate: new FormControl(),
+  //     brand: new FormControl(),
+  //     model: new FormControl(item?.model),
+  //     year: new FormControl(item?.year),
+  //   ])
+  // })
+
+  // setCars() {
+  //   if (this.cars) {
+  //     return this.cars.map(car => car);
+  //   } else {
+  //     return []
+  //   }
     // const carsFormArray = this.formBuilder.array(carsArray ? carsArray : []);
     // this.userCarsControl.setControl('carsControl', carsFormArray);
-  }
+  // }
 
   // createCar() {
   //   this.userCarsControl = this.formBuilder.group({
@@ -63,15 +94,15 @@ export class AddUserComponent implements OnInit {
   
 
 
-  createCar(item?: CarEntity): FormGroup {
-    // this.carsControl = this.userCarsControl.get('carsControl') as FormArray;
-    return this.formBuilder.group({
-      plate: new FormControl(item?.plate),
-      brand: new FormControl(item?.brand),
-      model: new FormControl(item?.model),
-      year: new FormControl(item?.year),
-    });
-  }
+  // createCar(item?: CarEntity): FormGroup {
+  //   // this.carsControl = this.userCarsControl.get('carsControl') as FormArray;
+  //   return this.formBuilder.group({
+  //     plate: new FormControl(item?.plate),
+  //     brand: new FormControl(item?.brand),
+  //     model: new FormControl(item?.model),
+  //     year: new FormControl(item?.year),
+  //   });
+  // }
 
   // addCar(item?: CarEntity): void {
   //   // this.carsControl = this.userCarsControl.get('carsControl') as FormArray;
